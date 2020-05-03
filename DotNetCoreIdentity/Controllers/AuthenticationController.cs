c306985af6cebabc2649298da34344a4e47347c8
Sinan Tok <sinantok@outlook.com>
2020-05-03 18:08:31 +0300

    Register endpoint

---
+++ DotNetCoreIdentity/Controllers/AuthenticationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreIdentity.Models.Identity;
+using DotNetCoreIdentity.Models.RequestModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetCoreIdentity.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Register([FromBody]UserRegister model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    return BadRequest(new { message = "UserName Already Registered." });
+                }
+
+                var applicationUser = new ApplicationUser 
+                { 
+                    UserName = model.UserName, 
+                    Name = model.Name, 
+                    LastName = model.LastName, 
+                    Email = model.Email 
+                };
+
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, model.Role);
+                    return Created("api/authentication/register", result);
+                }
+
+                return Ok(string.Join(",", result.Errors?.Select(error => error.Description)));
+            }
+
+            string errorMessage = string.Join(", ", ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage));
+            return BadRequest(errorMessage ?? "Bad Request");
+        }
+
+    }
+}
---
+++ DotNetCoreIdentity/Models/RequestModels/UserRegister.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreIdentity.Models.RequestModels
+{
+    public class UserRegister
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
